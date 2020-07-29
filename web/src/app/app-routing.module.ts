5f7f12125fe759cf6fdff7f7c1e0d63045d32977
José Rosendo <jose.l.rosendo.s@gmail.com>
2020-07-29 00:03:54 -0300

    remove unnecessary component import

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { TopicsComponent } from './topics/topics.component';
 
 const routes: Routes = [
