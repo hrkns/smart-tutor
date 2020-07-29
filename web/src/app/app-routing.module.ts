aa0a67164f7b9008757765939dc134496ab7242c
José Rosendo <jose.l.rosendo.s@gmail.com>
2020-07-28 23:27:30 -0300

    change base UI, continue later

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { TopicsComponent } from './topics/topics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'topics', component: TopicsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopicsComponent } from './topics/topics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopicsComponent
   ],
   imports: [
     BrowserModule,
